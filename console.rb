47fbb3ec7360da5ad61c72ac21cca8ebc84a5efa
Thomas Gracie <tgracie030@gmail.com>
2018-09-26 17:29:59 +0100

    Getting pg foreign key violation error when trying to run the console, will have another go later

--- console.rb
+++ console.rb
@@ -3,11 +3,23 @@ require_relative('./models/artist.rb')
 require('pry')
 
 Album.delete_all()
-# Artist.delete_all()
+Artist.delete_all()
+
+  rhcp = Artist.new({
+    'first_name' => 'Red Hot',
+    'last_name' => 'Chilli Peppers'
+    })
+    rhcp.save()
+
+    ezra = Artist.new({
+      'first_name' => 'George',
+      'last_name' => 'Ezra'
+      })
+      ezra.save
 
   stadium_arcadium = Album.new({
     'album_name' => 'Stadium Arcadium',
-    'genre' => 'rock'
+    'genre' => 'Rock'
     })
 
   staying_at_tams = Album.new({
@@ -17,6 +29,8 @@ Album.delete_all()
 
     stadium_arcadium.save()
     staying_at_tams.save()
+
+    # albums = Album.all()
 
     binding.pry
     nil
